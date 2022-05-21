3e2254d00fd645c541e22ee5b0212efde7e0192e
kyoyachuan <kyoyachuan0626@gmail.com>
2022-05-21 14:28:29 +0000

    feat: add preconditioning script

--- script.sh
+++ script.sh
@@ -1,1 +1,4 @@
-openssl rand -out /home/data/sample.txt -base64 792917038; truncate -s-1 /home/data/sample.txt
+openssl rand -out /home/kyoyachuan/data/sample.txt -base64 792917038; truncate -s-1 /home/kyoyachuan/data/sample.txt
+sudo mount -t tmpfs -o size=3G tmpfs /home/kyoyachuan/data/
+sudo swapoff -a && swapon -a
+sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
